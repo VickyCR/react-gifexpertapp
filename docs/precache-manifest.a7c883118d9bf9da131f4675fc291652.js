403bbb167f180a5983cfe86c4bb15bf3ff6ae5f6
vcastillonv <vcastillo@ids.com.mxm>
2020-09-09 18:31:59 -0500

    feat: docs updated

---
+++ docs/precache-manifest.a7c883118d9bf9da131f4675fc291652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5905149b37e46a5467ca4d63960af43c",
+    "url": "/react-gifexpertapp/index.html"
+  },
+  {
+    "revision": "d68b5c615225c1d2e5dd",
+    "url": "/react-gifexpertapp/static/css/main.a0c7c8f2.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/react-gifexpertapp/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gifexpertapp/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d68b5c615225c1d2e5dd",
+    "url": "/react-gifexpertapp/static/js/main.88af9342.chunk.js"
+  },
+  {
+    "revision": "8d60da8017703a69006d",
+    "url": "/react-gifexpertapp/static/js/runtime-main.ae19a04c.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28707ebe4fde7973ed48cb65e34d245a.js"
+  "/react-gifexpertapp/precache-manifest.a7c883118d9bf9da131f4675fc291652.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gifexpertapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
